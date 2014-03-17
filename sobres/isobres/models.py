b52b1e1370a38c909a34790abc93c84a004a5210
Anton U.C <auc2@hotmail.com>
2014-03-17 12:31:11 +0100

    First Part fixed

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Sobre(models.Model):
+class Sobres(models.Model):
 	date = models.DateTimeField()
 	amount = models.IntegerField()
 	concept = models.TextField(max_length=100)
